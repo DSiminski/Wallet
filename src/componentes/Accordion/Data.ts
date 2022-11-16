fd7271cf9964f1f230016aeeec566078a05ad5dc
Dayane Siminski <siminskidayane@gmail.com>
2022-11-16 15:39:18 -0300

    accordion

---
+++ src/componentes/Accordion/Data.ts
@@ -0,0 +1,14 @@
+export const Data = [
+    {
+        question: 'Despesas' ,
+        answer: 'button'
+    },
+    {
+        question: 'Pagamentos' ,
+        answer: 'button opcoes'
+    },
+    {
+        question: 'historico' ,
+        answer: 'histico'
+    },
+];
